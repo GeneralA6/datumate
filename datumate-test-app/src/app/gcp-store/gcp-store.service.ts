eff2963a1f2a54c40348783e9fe85c4e48c8c015
Ariel <ariel.estrin@gmail.com>
2022-04-12 21:01:58 +0300

    gcp store refactor - exposed gcpData + gcpFilenames observables + added remove file functionality

--- datumate-test-app/src/app/gcp-store/gcp-store.service.ts
+++ datumate-test-app/src/app/gcp-store/gcp-store.service.ts
@@ -1,16 +1,51 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { CsvFileData } from '../csv-file-uploader/csv-file-uploader.component';
 
 @Injectable({
-  providedIn: 'root'
+   providedIn: 'root'
 })
 export class GcpStoreService {
-  csvFiles: CsvFileData[] = [];
+   private gcpFilesStore = new BehaviorSubject(new Map<string, any>());
+   gcpData: Observable<any[]>;
+   gcpFiles: Observable<string[]>;
 
-  constructor() { }
+   constructor() {
+      this.gcpData = this.convertGcpFilesStoreToGcpDataObservable();
+      this.gcpFiles = this.convertGcpFilesStoreToFileNamesObservable();
+   }
 
-  addGcpFileData(csvFileData: CsvFileData) {
-    this.csvFiles.push(csvFileData);
-    console.log(this.csvFiles);
-  }
+   addGcpFileData(csvFileData: CsvFileData) {
+      const fileName: string = csvFileData.name;
+      const gcpFilesStoreSnapshot: Map<string, any> = this.gcpFilesStore.value;
+
+      if (gcpFilesStoreSnapshot.has(fileName)) {
+         return;
+      }
+      gcpFilesStoreSnapshot.set(fileName, csvFileData.data);
+
+      this.gcpFilesStore.next(gcpFilesStoreSnapshot);
+   }
+
+   removeGcpFileData(fileName: string) {
+      const gcpFilesStoreSnapshot: Map<string, any> = this.gcpFilesStore.value;
+
+      gcpFilesStoreSnapshot.delete(fileName);
+
+      this.gcpFilesStore.next(gcpFilesStoreSnapshot);
+   }
+
+   private convertGcpFilesStoreToGcpDataObservable(): Observable<any[]> {
+      return this.gcpFilesStore.asObservable().pipe(
+         map(gcpFilesStoreSnapshot => {
+            return [ ...gcpFilesStoreSnapshot ].map(([ key, value ]) => (value));
+         }));
+   }
+
+   private convertGcpFilesStoreToFileNamesObservable(): Observable<string[]> {
+      return this.gcpFilesStore.asObservable().pipe(
+         map(gcpFilesStoreSnapshot => {
+            return [ ...gcpFilesStoreSnapshot ].map(([ key, value ]) => (key));
+         }));
+   }
 }
